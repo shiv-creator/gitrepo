899ac677cf6fb4ffd580733194490b5b0c0390cb
shiv-creator <64470444+shiv-creator@users.noreply.github.com>
2020-05-05 13:04:16 +0530

    Update testprogram.java

--- src/test/java/testprogram.java
+++ src/test/java/testprogram.java
@@ -1,6 +1,6 @@
 import junit.framework.TestCase;
-public class CalculatorTest extends TestCase {
-Calculator cal=new Calculator();
+public class testprogram extends TestCase {
+program prg=new program();
 public void testadd() {
 assertEquals(2,cal.add(1,1));
 }
